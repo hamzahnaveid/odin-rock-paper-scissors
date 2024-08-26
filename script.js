e01bd01b233380427a0ab189645e9529a0f2c7cf
Hamzah Naveid <hamzahnaveid02@gmail.com>
2024-08-26 05:15:29 +0100

    Comment in tasks to complete

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1. Write the logic to get computer choice
+
+
+// 2. Write the logic to get the human choice
+
+
+// 3. Declare player score variables
+
+
+// 4. Write the logic to play a single round
+
+
+// 5. Write the logic to play the entire game
