0051ce0dba098028d762e941d984db6b0df98945
Hamzah Naveid <hamzahnaveid02@gmail.com>
2024-08-29 04:22:39 +0100

    Add function to take in human choice

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // 1. Write the logic to get computer choice
 function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1)
-    switch(compChoice) {
+    let computerChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1)
+    switch(computerChoice) {
         case 1 :
-            return 'rock'
+            return 'ROCK'
             break
         case 2 :
-            return 'paper'
+            return 'PAPER'
             break
         case 3 :
-            return 'scissor'
+            return 'SCISSORS'
     }
 }
 
 
 // 2. Write the logic to get the human choice
-
+function getHumanChoice() {
+    let humanChoice = prompt('Type in your choice (rock, paper, or scissors) \nN.B Spelling matters!')
+    humanChoice = humanChoice.toUpperCase()
+    if (humanChoice != 'ROCK' && humanChoice != 'PAPER' && humanChoice != 'SCISSORS')
+        {
+            alert('Invalid choice! Setting choice to ROCK')
+            return 'ROCK'
+        }
+    return humanChoice
+}
 
 // 3. Declare player score variables
 
