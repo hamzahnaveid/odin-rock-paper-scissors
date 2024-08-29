810ea287e6e978f431265afac69d55462f4568b7
Hamzah Naveid <hamzahnaveid02@gmail.com>
2024-08-29 04:41:47 +0100

    Add function to play round of rock-paper-scissors Two variables have been added to keep track of the human's score and the computer's score

--- script.js
+++ script.js
@@ -27,9 +27,57 @@ function getHumanChoice() {
 }
 
 // 3. Declare player score variables
-
+let humanScore = 0
+let computerScore = 0
 
 // 4. Write the logic to play a single round
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice == 'ROCK') {
+        switch(humanChoice) {
+            case 'ROCK' :
+                console.log('TIE! No points awarded')
+                break
+            case 'PAPER' :
+                console.log('YOU WIN! PAPER beats ROCK')
+                humanScore++
+                break
+            case 'SCISSORS' :
+                console.log('YOU LOSE! ROCK beats SCISSORS')
+                computerScore++
+                break
+        }
+    }
+    else if (computerChoice == 'PAPER') {
+        switch(humanChoice) {
+            case 'ROCK' :
+                console.log('YOU LOSE! PAPER beats ROCK')
+                computerScore++
+                break
+            case 'PAPER' :
+                console.log('TIE! No points awarded')
+                break
+            case 'SCISSORS' :
+                console.log('YOU WIN! SCISSORS beats PAPER')
+                humanScore++
+                break
+        }
+    }
+    else { //when computerChoice is SCISSORS
+        switch(humanChoice) {
+            case 'ROCK' :
+                console.log('YOU WIN! ROCK beats SCISSORS')
+                humanScore++
+                break
+            case 'PAPER' :
+                console.log('YOU LOSE! SCISSORS beats PAPER')
+                computerScore++
+                break
+            case 'SCISSORS' :
+                console.log('TIE! No points awarded')
+                break
+        }
+    }
+}
 
 
 // 5. Write the logic to play the entire game
